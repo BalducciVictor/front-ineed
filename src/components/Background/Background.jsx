7dfcaf2d579904136f0868723c7ad6cfa7491c38
Balducci Victor <Victor@MacBook-Pro-de-Balducci.local>
2020-02-12 15:45:55 +0100

    add profile if exist/ fix css bug

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import './Background.scss';
+import axios from 'axios';
 
 import ShowProfile from '../ShowProfile/ShowProfile';
 import AddProfile from '../AddProfile/AddProfile';
 import Map from '../Map/Map';
 
 class Background extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.state = {
+      profils: []
+    }
+    this.clearUserData = this.clearUserData.bind(this);
   }
+  componentDidMount(){
+    this.call_getProfile();
+  }
+  //Call to retrieve profils by ID
+  call_getProfile(){
+    let data = {User: sessionStorage.getItem('id')};
+    const configSend = {'Content-Type': 'application/json'}
+    axios.get('http://13.59.220.41/api/profils', data, configSend)
+      .then (res => {
+        let profils = res.data['hydra:member'];
+        profils = this.clearUserData(profils);
+        console.log(profils)
+        this.setState({
+          profils : profils
+        })
+      })
+      .catch (err => {
+        // console.log (err.response.data);
+      })
+  }
+  clearUserData(profils){
+    return profils.map( (profil) => {
+      const {name,picture} = profil
+      return {
+        name,
+        picture,
+      }
+    });
+  }
+  //<ShowProfile/>
   render(){
+    const { profils } = this.state
     return(
       <div className="background-profiles">
         <svg className="logo_home"><use xlinkHref="/many_svg.svg#logo"/></svg>
@@ -17,7 +53,9 @@ class Background extends React.Component {
         <div className="boxWrapper-p">
           <h1 className="title-profile">Welcome</h1>
           <div className="list-profiles">
-            <ShowProfile/>
+             { profils.length ? profils.map( (profil, i) => {
+                return <ShowProfile key={'profil' + profil.name + i} name={profil.name} picture={profil.picture} /> 
+             }): ''}
             <AddProfile/>
             <Map/>
           </div>
--- src/components/ShowProfile/ShowProfile.jsx
+++ src/components/ShowProfile/ShowProfile.jsx
@@ -5,20 +5,19 @@ import './ShowProfile.scss';
 import paul from '../../assets/paul.jpg';
 
 
-const ShowProfile = () => (
+const ShowProfile = (props) => (
   <div className="profile">
     <div className="remove-profile">
       <a>Remove</a>
     </div>
     <div className="flex--center">
       <div>
-      <img className="user" src={paul} alt="Picture user"/>
-      <h2 className="profile-name">Paul CHARLES</h2>
+      <img className="user" src={props.picture} alt="Picture user"/>
+      <h2 className="profile-name">{props.name}</h2>
       </div>
     </div>
     <button className="profile-button" href="#">See the profile</button>
   </div>
 );
-
 
 export default ShowProfile;
