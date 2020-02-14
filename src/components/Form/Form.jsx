0a45ae0ae0d24c8e921028640947b358b3326c83
cheik <cheik.kone@hetic.net>
2020-02-14 11:39:30 +0100

    auto

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React,{ useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Form.scss'
 import DisconnectButton from '../Buttons/DisconnectButton/DisconnectButton'
 import ShowProfileButton from '../Buttons/ShowProfileButton/ShowProfileButton'
@@ -6,77 +6,76 @@ import arrow from '../../assets/img/arrow-back.png'
 import Api from '../../Api/Api'
 import ShowProfile from '../ShowProfile/ShowProfile'
 
-function Form() {
+function Form () {
+  // data form
+  const [gender, setGender] = useState('')
+  const [name, setName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [age, setAge] = useState('')
+  const [bloodType, setBloodType] = useState('')
+  const [chronicDiseases, setChronicDiseases] = useState('')
+  const [drugs, setDrugs] = useState('')
+  const [moreInformation, setMoreInformation] = useState('')
 
-  //data form
-  const [gender, setGender ] = useState('')
-  const [ name, setName ] = useState('');
-  const [ firstName, setFirstName ] = useState('');
-  const [ age, setAge ] = useState('');
-  const [ bloodType, setBloodType ] = useState('');
-  const [ chronicDiseases, setChronicDiseases ] = useState('');
-  const [ drugs, setDrugs ] = useState('');
-  const [ moreInformation, setMoreInformation ] = useState('');
-
-  //data API
-  const [ chronicDisease, setChronicDisease ] = useState([]);
+  // data API
+  const [chronicDisease, setChronicDisease] = useState([])
 
   const apiRequest = new Api()
 
-  //modification state chronic disease
+  // modification state chronic disease
   useEffect(() => {
-    getDataSchronicDisease();
-  });
+    getDataSchronicDisease()
+  })
 
-  //get data Chronic Disease
+  // get data Chronic Disease
   const getDataSchronicDisease = () => {
-    if ( !chronicDisease.length ) {
+    if (!chronicDisease.length) {
       apiRequest.get('/api/maladie_chroniques')
-      .then(res => {
-        let ChronicDiseases = res.data['hydra:member']
-        setChronicDisease(res.data['hydra:member'])
-      })
+        .then(res => {
+          const ChronicDiseases = res.data['hydra:member']
+          setChronicDisease(res.data['hydra:member'])
+        })
     }
   }
-  //add chronic disease option in select
+  // add chronic disease option in select
   const listItems = chronicDisease.map((data) =>
-  <option key={data.name.toString()}>{data.name}</option>
-  );
+    <option key={data.name.toString()}>{data.name}</option>
+  )
 
   const postDataNewProfil = (e) => {
-      e.preventDefault();
-      alert('caroule')
-      const data = {
-        "name": name,
-        "surname": firstName,
-        "gender": gender,
-        "birthDate": age,
-        "bloodType": bloodType,
-        "picture": "yolo",
-        "information": moreInformation,
-        "User": "string",
-        "medicaments": [
-          drugs
-        ],
-        "maladieChroniques": [
-          chronicDiseases
-        ],
-      }
-      apiRequest.post(`/api/profils/${'coucou'}`, data)
+    e.preventDefault()
+    alert('caroule')
+    const data = {
+      name: name,
+      surname: firstName,
+      gender: gender,
+      birthDate: age,
+      bloodType: bloodType,
+      picture: 'yolo',
+      information: moreInformation,
+      User: 'string',
+      medicaments: [
+        drugs
+      ],
+      maladieChroniques: [
+        chronicDiseases
+      ]
+    }
+    apiRequest.post(`/api/profils/${'coucou'}`, data)
       .then(res => {
         console.log(res, 'tout vas bien')
       })
-       .catch(err => {
-            console.log(err, 'cest la merde')   
-     })
+      .catch(err => {
+        console.log(err, 'cest la merde')
+      })
   }
-  
+
   return (
-  <div className="background">
-    <svg className="logo_home"><use xlinkHref="/many_svg.svg#logo"/></svg>
-    <DisconnectButton/>
-    <ShowProfileButton/>
-    <div className="boxWrapper">
+    <div className="background">
+      <svg className="logo_home"><use xlinkHref="/many_svg.svg#logo"/></svg>
+      <DisconnectButton/>
+      <ShowProfileButton/>
+      <div className="boxWrapper">
         <h1 className="title-form">My new profile</h1>
         <div className="wrap-form">
           <div className='back-to-home'>
@@ -88,9 +87,9 @@ function Form() {
             <label>Gender</label>
             <div>
               <label>Male</label>
-              <input type="radio" checked={ gender === 'Male' ?  true : false} onChange={() => setGender('h')}/>
+              <input type="radio" checked={ gender === 'Male'} onChange={() => setGender('h')}/>
               <label>Female</label>
-              <input type="radio" checked={ gender === 'female' ?  true : false} onChange={() => setGender('f')}/>
+              <input type="radio" checked={ gender === 'female'} onChange={() => setGender('f')}/>
             </div>
             <div>
               <label>Name</label>
@@ -106,17 +105,17 @@ function Form() {
             </div>
             <div>
               <label>Blood type</label>
-                <select onChange={(e) => setBloodType(e.target.value)}>
-                  <option value=""></option>
-                  <option value="O-">O-</option>
-                  <option value="O+">O+</option>
-                  <option value="B-">B-</option>
-                  <option value="B+">B+</option>
-                  <option value="A-">A-</option>
-                  <option value="A+">A+</option>
-                  <option value="AB-">AB-</option>
-                  <option value="AB+">AB+</option>
-                </select>
+              <select onChange={(e) => setBloodType(e.target.value)}>
+                <option value=""></option>
+                <option value="O-">O-</option>
+                <option value="O+">O+</option>
+                <option value="B-">B-</option>
+                <option value="B+">B+</option>
+                <option value="A-">A-</option>
+                <option value="A+">A+</option>
+                <option value="AB-">AB-</option>
+                <option value="AB+">AB+</option>
+              </select>
             </div>
             <div>
               <label>Chronic disease</label>
@@ -129,7 +128,7 @@ function Form() {
               <input type="text" placeholder="Acetylleucine mylan 500" onChange={(e) => setDrugs(e.target.value)}/>
             </div>
             <div>
-            <label>More information</label>
+              <label>More information</label>
               <textarea onChange={(e) => setMoreInformation(e.target.value)} />
             </div>
             <input type="submit" value="Save" onClick={(e) => postDataNewProfil(e)}/>
@@ -137,11 +136,7 @@ function Form() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-<<<<<<< HEAD
 export default Form
-=======
-export default Form
->>>>>>> victor
