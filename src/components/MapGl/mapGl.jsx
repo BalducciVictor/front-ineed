ff5bc8a4bb97bd22cd076ea34a3d484d6508605c
cheik <cheik.kone@hetic.net>
2020-02-14 09:22:04 +0100

    add map

---
+++ src/components/MapGl/mapGl.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl'
+import pictoPharmacie from '../../assets/picto/ping-pharmacie.svg'
+
+require('dotenv').config()
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2hlaWsiLCJhIjoiY2s2a3pzMDE2MDk0azNucGF3cHI1bjhsZiJ9.O5OgNMJeOXTZTVfv7kAuwA'
+
+function Map ({ phrmacies }) {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: 600,
+    latitude: 48.8534,
+    longitude: 2.3488,
+    zoom: 10.9,
+    captureScroll: true,
+    events: []
+  })
+
+  // MAPBOX_TOKEN
+
+  return (
+    <ReactMapGL {...viewport} onViewportChange={viewport => { setViewport(viewport) }} mapboxApiAccessToken={MAPBOX_TOKEN}>
+
+      {
+        phrmacies ? phrmacies.map(({ latitude, longitude }, i) => <Marker key={`KEY$__${i}`} latitude={Number(latitude)} longitude={Number(longitude)} >
+          <img src={pictoPharmacie}></img>
+        </Marker>) : ''
+      }
+    </ReactMapGL>
+  )
+}
+
+export default Map
--- src/pages/Datavisualitaion/Datavisualisation.jsx
+++ src/pages/Datavisualitaion/Datavisualisation.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import axios from 'axios';
-import Datvis from './dataVis.jsx';
+import React from 'react'
+import axios from 'axios'
+import Datvis from './dataVis.jsx'
 import './styleDataVis.scss'
 // import SwitchButton from '../../components/Buttons/SwitchButton/SwitchButton';
-import MedicalProfile from '../../components/MedicalProfile/MedicalProfile';
-
+import MedicalProfile from '../../components/MedicalProfile/MedicalProfile'
+import MapGl from '../../components/MapGl/mapGl'
 
 class PersonalInformation extends React.Component {
-
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
+      phrmacies: null,
       data: null,
       centres: null,
       specialites: null,
-      dataClean: null,
-    };
-  }
-
-  async componentDidMount() {
-    axios.get('http://13.59.220.41/api/arrondissements')
-      .then((res) => {
-        this.setState({
-          data: res.data['hydra:member'],
-        });
-      });
-  }
-
-
-  render() {
-    const { data } = this.state;
-      return (
-        <div className="personal-information datavis">
-          <svg className="logo_home"><use xlinkHref="/many_svg.svg#logo"/></svg>
-          <button className="specialty-button">search a specialty</button>
-          <div className="boxWrapper-i">
-            <h1 className="title-information">My profile</h1>
-            <div className="wrap-information">
-            </div>
-            { data ? <Datvis data={data} /> : <div /> }
-          </div>
-        </div>
-      );
+      dataClean: null
     }
   }
 
-export default PersonalInformation;
+  async componentDidMount () {
+    axios.get('http://13.59.220.41/api/arrondissements')
+      .then((res) => {
+        this.setState({
+          data: res.data['hydra:member']
+        })
+      })
+    axios.get('http://13.59.220.41/api/pharmacies?Arrondissement=14&Arrondissement[]=1%2C2%2C3%2C4%2C5%2C6%2C7%2C8%2C9%2C10%2C11%2C12%2C13%2C14%2C16%2C17%2C18%2C19%2C20')
+      .then((res) => {
+        this.setState({
+          phrmacies: res.data['hydra:member']
+        })
+        console.log(res)
+      })
+  }
+
+  render () {
+    const { data, phrmacies } = this.state
+    return (
+      <div className="personal-information datavis">
+        <svg className="logo_home"><use xlinkHref="/many_svg.svg#logo"/></svg>
+        <button className="specialty-button">search a specialty</button>
+        <div className="boxWrapper-i">
+          <h1 className="title-information">My profile</h1>
+          <div className="wrap-information">
+            <MapGl phrmacies={phrmacies} ></MapGl>
+          </div>
+          { data ? <Datvis data={data} /> : <div /> }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PersonalInformation
