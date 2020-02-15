e39953bf3caad0bf518bbbfcc63b07e77deb26da
cheik <cheik.kone@hetic.net>
2020-02-15 22:18:20 +0100

    áuto

--- src/components/Buttons/SwitchButton/SwitchButton.jsx
+++ src/components/Buttons/SwitchButton/SwitchButton.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 
-export default ({ setNewSwitch, switchState }) => {
+const switchButton = ({ setNewSwitch, switchState }) => {
   return (
     <div className={`switch-button ${switchState === 0 ? 'left' : 'right'}`}>
-      <label>
-        My list
-        <input name="radio" type="radio" onClick={() => { setNewSwitch(0) }} value="optionone" id="optionone" checked={ switchState === 0 }/>
-      </label>
-      <label className="right">
-        My information
-        <input name="radio" type="radio" value="optiontwo" onClick={() => { setNewSwitch(1) }} id="optiontwo" checked={ switchState === 1 }/>
-      </label>
+      <div onClick={() => { setNewSwitch(1) }}>My list</div>
+      <div onClick={() => { setNewSwitch(1) }}>My information</div>
     </div>
   )
 }
+
+export default switchButton
