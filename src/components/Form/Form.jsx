c07e3030e0db142797b3e48f359573cb78ea1ff9
kryweb <dreidemyromain@gmail.com>
2020-02-15 19:02:14 +0100

    Création de nouveaux profils

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,71 +4,77 @@ import DisconnectButton from '../Buttons/DisconnectButton/DisconnectButton'
 import ShowProfileButton from '../Buttons/ShowProfileButton/ShowProfileButton'
 import arrow from '../../assets/img/arrow-back.png'
 import Api from '../../Api/Api'
+import ChronicDiseasesSelect from './select/ChronicDiseasesSelect'
 import ShowProfile from '../ShowProfile/ShowProfile'
 
 function Form () {
-  // data form
-  const [gender, setGender] = useState('')
-  const [name, setName] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [age, setAge] = useState('')
-  const [bloodType, setBloodType] = useState('')
-  const [chronicDiseases, setChronicDiseases] = useState('')
-  const [drugs, setDrugs] = useState('')
-  const [moreInformation, setMoreInformation] = useState('')
-
-  // data API
-  const [chronicDisease, setChronicDisease] = useState([])
 
   const apiRequest = new Api()
 
-  // modification state chronic disease
-  useEffect(() => {
-    getDataSchronicDisease()
-  })
-
-  // get data Chronic Disease
-  const getDataSchronicDisease = () => {
-    if (!chronicDisease.length) {
-      apiRequest.get('/api/maladie_chroniques')
-        .then(res => {
-          const ChronicDiseases = res.data['hydra:member']
-          setChronicDisease(res.data['hydra:member'])
-        })
-    }
-  }
-  // add chronic disease option in select
-  const listItems = chronicDisease.map((data) =>
-    <option key={data.name.toString()}>{data.name}</option>
-  )
-
   const postDataNewProfil = (e) => {
-    e.preventDefault()
-    alert('caroule')
+    e.preventDefault();
+
+    let recupData = e.target.elements;
+
+    console.log(recupData.chronicDiseases.value);
+
     const data = {
-      name: name,
-      surname: firstName,
-      gender: gender,
-      birthDate: age,
-      bloodType: bloodType,
+      name: recupData.name.value,
+      surname: recupData.surname.value,
+      gender: recupData.gender.value,
+      birthDate: recupData.birthDate.value,
+      bloodType: recupData.bloodType.value,
       picture: 'yolo',
-      information: moreInformation,
-      User: 'string',
-      medicaments: [
-        drugs
-      ],
-      maladieChroniques: [
-        chronicDiseases
-      ]
+      information: recupData.information.value,
+      User: '/api/users/' + window.sessionStorage.id,
+      maladieChroniques: []
+    };
+
+    if(recupData.chronicDiseases.value !== ''){
+      if(typeof recupData.chronicDiseases[0] != 'undefined'){
+        recupData.chronicDiseases.forEach(d => {
+          data.maladieChroniques.push('/api/maladie_chroniques/' + d.value);
+        })
+      }else{
+        data.maladieChroniques.push('/api/maladie_chroniques/' + recupData.chronicDiseases.value);
+      }
     }
-    apiRequest.post(`/api/profils/${'coucou'}`, data)
-      .then(res => {
-        console.log(res, 'tout vas bien')
-      })
-      .catch(err => {
-        console.log(err, 'cest la merde')
-      })
-  }
+
+
+    console.log('data : ');
+    console.log(data);
+    let idProfil = '';
+  //  Envoi du profil dans l'api dans l'api
+    apiRequest.post('/api/profils', data)
+        .then((response) => {
+          idProfil = response.data.id;
+          console.log(response.data)
+
+          //  TODO création des médicaments
+          if(idProfil !== ''){
+            const dataDrugs = {
+              name: recupData.drug.value,
+              Profil: '/api/profils/' + idProfil
+            };
+
+            apiRequest.post('/api/medicaments', dataDrugs)
+                .then((response) => {
+                  console.log(response.data)
+                  window.location.pathname = '/profile'
+                })
+                .catch((error) => {
+                  console.log(error)
+                })
+          }else {
+            console.log('pas idProfil')
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+    }
+
+
 
   return (
     <div className="background">
@@ -82,32 +88,32 @@ function Form () {
             <img className="arrow-back" src={arrow} alt="Arrow back home"/>
             <h2 className="back">Back to home</h2>
           </div>
-          <form className="form-profile">
+          <form className="form-profile" onSubmit={(e) => {postDataNewProfil(e)}}>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <label>Gender</label>
             <div>
               <label>Male</label>
-              <input type="radio" checked={ gender === 'Male'} onChange={() => setGender('h')}/>
+              <input checked type="radio" name="gender" value="m"/>
               <label>Female</label>
-              <input type="radio" checked={ gender === 'female'} onChange={() => setGender('f')}/>
+              <input type="radio" name="gender" value="f"/>
             </div>
             <div>
               <label>Name</label>
-              <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Dupont" required/>
+              <input type="text" name="name" value="Dreidemy" required/>
             </div>
             <div>
               <label>First Name</label>
-              <input type="text" onChange={(e) => setFirstName(e.target.value)} placeholder="Martin" required/>
+              <input type="text" name="surname" value="Romain" required/>
             </div>
             <div>
               <label>Age</label>
-              <input type="number" onChange={(e) => setAge(e.target.value)} placeholder="46" required/>
+              <input type="date" name="birthDate" value="1999-06-19" placeholder="46" required/>
             </div>
             <div>
               <label>Blood type</label>
-              <select onChange={(e) => setBloodType(e.target.value)}>
+              <select name="bloodType">
                 <option value=""></option>
-                <option value="O-">O-</option>
+                <option selected value="O-">O-</option>
                 <option value="O+">O+</option>
                 <option value="B-">B-</option>
                 <option value="B+">B+</option>
@@ -119,19 +125,17 @@ function Form () {
             </div>
             <div>
               <label>Chronic disease</label>
-              <select onChange={(e) => setChronicDiseases(e.target.value)}>
-                {listItems}
-              </select>
+              <ChronicDiseasesSelect/>
             </div>
             <div>
               <label>Drug</label>
-              <input type="text" placeholder="Acetylleucine mylan 500" onChange={(e) => setDrugs(e.target.value)}/>
+              <input type="text" name="drug" value="Acetylleucine mylan 500" />
             </div>
             <div>
               <label>More information</label>
-              <textarea onChange={(e) => setMoreInformation(e.target.value)} />
+              <textarea name="information">Quelques informations</textarea>
             </div>
-            <input type="submit" value="Save" onClick={(e) => postDataNewProfil(e)}/>
+            <input type="submit" />
           </form>
         </div>
       </div>
