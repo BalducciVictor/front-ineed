31ba6c696103e66154fde4b60f1757bc2bb84af1
Balducci Victor <Victor@MacBook-Pro-de-Balducci.local>
2020-02-25 11:03:57 +0100

    small modification Form css

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -77,7 +77,7 @@ function Form () {
           <form className="form-profile" onSubmit={(e) => { postDataNewProfil(e) }}>
             {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p> */}
             <label>Gender</label>
-            <div>
+            <div className="gender-checkbox">
               <label>Male</label>
               <input checked type="radio" name="gender" value="m"/>
               <label>Female</label>
