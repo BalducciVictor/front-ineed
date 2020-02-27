319bdbb7818b83dc94578f0933b16367f5fee1e2
emmacassagnettes <e.cassagnettes@gmail.com>
2020-02-27 10:02:55 +0100

    pop

--- src/components/PopAddListProfile/PopAddListProfile.jsx
+++ src/components/PopAddListProfile/PopAddListProfile.jsx
@@ -2,7 +2,45 @@ import React, { useEffect, useState } from 'react'
 import ShowProfile from '../ShowProfile/ShowProfile'
 import axios from 'axios'
 
-const PopAddListProfile = () => {
+
+
+  const [profils, setProfils] = useState([])
+
+  const clearUserData = (profils) => {
+    return profils.map((profil) => {
+      const { id, name, surname } = profil;
+      return {
+        id,
+        name,
+        surname
+      }
+    })
+  };
+
+  const callGetProfile = () => {
+    axios.get('http://13.59.220.41/api/profils?User=' + sessionStorage.getItem('id'))
+      .then(res => {
+        let profilsFromData = res.data['hydra:member'];
+        profilsFromData = clearUserData(profilsFromData);
+        setProfils(profilsFromData);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      })
+  };
+
+
+  useEffect(() => {
+    if (!profils.length) {
+      callGetProfile();
+    }
+  });
+  const PopAddListProfile = () => {
+  return (
+    <div>
+      { profils.length ? <ShowProfile key={profils.id} name={profils.name} surname={profils.surname} id={profils.id} /> : ''}
+    </div>
+  )
 
 }
 
